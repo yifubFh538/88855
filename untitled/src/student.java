51bb20a7b2cd3e5b17e4e5031fb0ea54300cd9ac
Bs34623 <1004061791@qq.com>
2022-07-27 11:17:50 +0800

    b

---
+++ untitled/src/student.java
@@ -0,0 +1,24 @@
+public class student {
+    /**
+     * 行为
+     * 上晚自习
+     * 泡图书馆
+     * 上课
+     * 做广播体操
+     * f（X)=ax+b
+     */
+
+}
+    public void goToNightclasses(){
+
+        System.out.println("我们在认真上晚自习");
+    }
+    public void bubbleLibrary(){
+        System.out.println("我们在泡图qqq书馆");
+    }
+    public void attendClass(){
+        System.out.println("我们在认真上课");
+    }
+    public void doRadioGymnastics(){
+        System.out.println("我们在认真做广播体操");
+    }
